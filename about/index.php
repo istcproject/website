bc115d21ada90db85bc919db22887381e33db7d7
istcproject <112457214+istcproject@users.noreply.github.com>
2022-08-30 19:49:53 +0530

    photos

---
+++ about/index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en" >
+<head>
+  <meta charset="UTF-8">
+  <title>About ISTC</title>
+  <link rel="stylesheet" href="./style.css">
+
+</head>
+<body>
+<!-- partial:index.partial.html -->
+<header class="site-header">
+  <div class="site-identity"
+  >
+    <h1><a href="#">About ISTC</a></h1>
+  </div>
+  <nav class="site-navigation">
+    <div class="nav">
+       <button class="custom-btn btn-11"><span>About ISTC</span></button>
+        <button class="custom-btn btn-11">Gallery</button>
+          <button class="custom-btn btn-11">Courses</button>
+
+         <button class="custom-btn btn-11">Credits</button>
+    <button class="custom-btn btn-15">HOME</button>
+
+    </div>
+  </nav>
+</header>
+<!-- partial -->
+<div style="background-color:blue; height:200px;">
+
+</div>
+<section><p>hii</p>
+  <?php
+  echo "<h2>PHP is Fun!</h2>";
+  echo "Hello world!<br>";
+  echo "I'm about to learn PHP!<br>";
+  echo "This ", "string ", "was ", "made ", "with multiple parameters.";
+  ?></section>
+
+</body>
+</html>
